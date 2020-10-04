e620466d2bb9cdb4891d0c846f69bb3efcbfea53
Esger Jellema <esgerj@gmail.com>
2020-10-04 13:14:44 +0200

    Fix error with runtime.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // regenerator-runtime is to support async/await syntax in ESNext.
 // If you don't use async/await, you can remove regenerator-runtime.
-import 'regenerator-runtime/runtime';
+// import 'regenerator-runtime/runtime';
 import environment from './environment';
 
 export function configure(aurelia) {
